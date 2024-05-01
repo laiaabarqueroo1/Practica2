f01e9a833f4979de8b710a202bed6aed204fb96a
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-05-01 15:17:33 +0200

    classePunt

--- js/punt.js
+++ js/punt.js
@@ -1,14 +1,12 @@
-/*
-* CLASSE PUNT
-*/
+/* CLASSE PUNT */
 
 class Punt {
     constructor(x, y,) {
         this.x = x;
         this.y = y;
     }
+
     static distanciaDosPunts(a, b) {
-        
         return Math.sqrt(Math.pow(a.x-b.x,2)+ Math.pow(a.y-b.y,2));
     }
 }
