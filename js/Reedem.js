c6c159bf4f008336dee7c74557bbd27e25f3f6b5
laiaabarqueroo1 <166661334+laiaabarqueroo1@users.noreply.github.com>
2024-05-28 01:39:20 +0200

    retocando cosas del css

--- js/Reedem.js
+++ js/Reedem.js
@@ -1,29 +1,10 @@
-/*// Datos de los items
-const items = {
-    heart: {
-        name: "Incrementar Vidas",
-        description: "Aumenta una vida extra en el juego.",
-        cost: 100
-    },
-    time: {
-        name: "Tiempo Extra",
-        description: "Agrega tiempo adicional al cronómetro.",
-        cost: 50
-    },
-    points: {
-        name: "Doble Puntuación",
-        description: "Duplica los puntos obtenidos por un tiempo limitado.",
-        cost: 150
-    }
-};
-*/
-// Producto a favoritos
-$('.card .aFavs').click(function(){
-    $(this).parents('.card').toggleClass('esFav');
-});
+const addButtons = document.querySelectorAll('.add-button');
 
-// Producto al carrito
-$('.card .alCarrito').click(function(){
-    $(this).parents('.card').toggleClass('enCarrito');
-});
   
+  addButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      button.classList.toggle('active');
+      const card = button.closest('.rounded-lg');
+      card.classList.toggle('redeem-active');
+    });
+  });
