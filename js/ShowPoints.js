bab3ee21b422b62a975d0356ceedbf2eed87c3cb
laiaabarqueroo1 <166661334+laiaabarqueroo1@users.noreply.github.com>
2024-05-28 08:51:28 +0200

    score correcte

--- js/ShowPoints.js
+++ js/ShowPoints.js
@@ -1,6 +1,6 @@
 
-    var userName = getCookie('loggedInUser');
-    var users = JSON.parse(localStorage.getItem('users')) || [];
+    /*var userName = getCookie('loggedInUser');
+    var users = JSON.parse(localStorage.getItem('users'));
     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
     if (currentUser) {
@@ -32,3 +32,22 @@
         localStorage.setItem('currentUser', JSON.stringify(currentUser));
     }
 
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    // Recuperar los datos de puntuaciones acumuladas del almacenamiento local
+    var users = JSON.parse(localStorage.getItem('users')) || {};
+    
+    // Mostrar los datos en la página
+    for (var userName in users) {
+        var userScore = users[userName].score;
+        var userLevel = users[userName].level;
+        
+        // Mostrar el puntaje del usuario en la sección "Your Current Score"
+        var scoreElement = document.querySelector('.user-points .score');
+        scoreElement.textContent = userScore + ' points';
+
+        // Mostrar el nivel del usuario en la sección "Your Level"
+        var levelElement = document.querySelector('.user-level .level');
+        levelElement.textContent = userLevel;
+    }
+});
