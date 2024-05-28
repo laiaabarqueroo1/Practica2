4ed4fe177e687ca37f3cbdae73a233fb7838a378
laiaabarqueroo1 <166661334+laiaabarqueroo1@users.noreply.github.com>
2024-05-28 09:20:57 +0200

    FUNCIOINA SHOWPOINTS

--- js/ShowPoints.js
+++ js/ShowPoints.js
@@ -39,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
     
     // Mostrar los datos en la página
     for (var userName in users) {
-        var userScore = users[userName].score;
+        var userScore = users[userName].games;
         var userLevel = users[userName].level;
         
         // Mostrar el puntaje del usuario en la sección "Your Current Score"
