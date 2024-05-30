920cf4bdb575015c954d483c7edda8acfa0302d8
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-05-30 13:20:49 +0200

    paddle reduïda

--- js/Game.js
+++ js/Game.js
@@ -13,7 +13,7 @@ class Game {
         this.usedLives = [];
         this.currentLevel = currentLevel;      
 
-        this.paddle = new Paddle(new Point((this.canvas.width - 60) / 2, this.height - 15), 60, 4);
+        this.paddle = new Paddle(new Point((this.canvas.width - 50) / 2, this.height - 15), 50, 4);
         this.ball = new Ball(new Point(this.canvas.width / 2, 130), 3);
         this.wall = new Wall(this.width, this.height, this.brickWidth, this.brickHeight, this.currentLevel);
 
