0e6badc555adb9400e6ec65fe75a4e12fba3f3be
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-05-30 13:25:09 +0200

    bucle música

--- js/Game.js
+++ js/Game.js
@@ -40,10 +40,14 @@ class Game {
     initialize() {
         this.draw();
 
-
         // Background Music
         const BackgroundMusic = new Audio('./sounds/BackgroundMusic.mp3');
         BackgroundMusic.volume = 0.3; // Adjust the background music volume (0.0 - 1.0)
+
+        BackgroundMusic.addEventListener('ended', () => {
+            BackgroundMusic.play();
+        });
+
         BackgroundMusic.play();
 
         let isMusicPlaying = true; 
