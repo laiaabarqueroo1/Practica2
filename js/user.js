37bccf55b6757b2064a1e96bbae6ee911e522dbd
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-06-04 09:14:26 +0200

    Revert "Update user.js"
    This reverts commit 0a6e49e57c3704982789d56f7f1fe013e04015d3.

--- js/user.js
+++ js/user.js
@@ -24,7 +24,7 @@ class User {
     setCookie(name, value, days) {
         const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        document.cookie = ${name}=${value};expires=${date.toUTCString()};path=/;
+        document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
     }
 
     getCookie(name) {
