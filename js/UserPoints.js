bc8baff220f5d4e40a2f4ed8ba366977501a7a4c
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-06-05 09:06:12 +0200

    Update UserPoints.js

--- js/UserPoints.js
+++ js/UserPoints.js
@@ -11,13 +11,13 @@ class UserPoints {
     }
 
     displayPoints() {
-<<<<<<< HEAD
-        if (localStorage.getItem('username')) {
-            const username = localStorage.getItem('username');
-            this.element.textContent = `Points: ${this.users[username].totalScore}`;
-        } else {
-            this.element.textContent = `Points: ${this.points}`;
+        this.points = 0;
+        for (const username in this.users) {
+            if (this.users.hasOwnProperty(username)) {
+                this.points += this.users[username].totalScore;
+            }
         }
+        this.element.textContent = `Points: ${this.points}`;        
     }
 
     redeemProduct(selectedCardData) {
@@ -34,29 +34,6 @@ class UserPoints {
                 this.users[username].redeemedProducts[selectedCardData.id]++;
             } else {
                 this.users[username].redeemedProducts[selectedCardData.id] = 1;
-=======
-        this.element.textContent = `Points: ${this.points}`;
-    }
-
-    redeemProduct(selectedCardData) {
-        const pointsToRedeem = parseInt(selectedCardData.points);
-        
-        if (this.points >= pointsToRedeem) {
-            this.points -= pointsToRedeem;
-            alert(`You have redeemed ${selectedCardData.title} for ${pointsToRedeem} points.`);
-
-            for (const userName in this.users) {
-                if (this.users.hasOwnProperty(userName)) {
-                    this.users[userName].totalScore = this.points;
-                    this.users[userName].redeemedProducts = this.users[userName].redeemedProducts || {};
-                    
-                    if (this.users[userName].redeemedProducts[selectedCardData.id]) {
-                        this.users[userName].redeemedProducts[selectedCardData.id]++;
-                    } else {
-                        this.users[userName].redeemedProducts[selectedCardData.id] = 1;
-                    }
-                }
->>>>>>> parent of bc19a47 (Update UserPoints.js)
             }
             
             localStorage.setItem('users', JSON.stringify(this.users));
