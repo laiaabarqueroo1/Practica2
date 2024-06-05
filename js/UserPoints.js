96abd7d31d211ccf98e0ba64e3c12f2b0cfb6347
AinaBarnet <147982964+AinaBarnet@users.noreply.github.com>
2024-06-05 09:02:22 +0200

    revert

--- js/UserPoints.js
+++ js/UserPoints.js
@@ -11,6 +11,7 @@ class UserPoints {
     }
 
     displayPoints() {
+<<<<<<< HEAD
         if (localStorage.getItem('username')) {
             const username = localStorage.getItem('username');
             this.element.textContent = `Points: ${this.users[username].totalScore}`;
@@ -33,8 +34,31 @@ class UserPoints {
                 this.users[username].redeemedProducts[selectedCardData.id]++;
             } else {
                 this.users[username].redeemedProducts[selectedCardData.id] = 1;
-            }
+=======
+        this.element.textContent = `Points: ${this.points}`;
+    }
 
+    redeemProduct(selectedCardData) {
+        const pointsToRedeem = parseInt(selectedCardData.points);
+        
+        if (this.points >= pointsToRedeem) {
+            this.points -= pointsToRedeem;
+            alert(`You have redeemed ${selectedCardData.title} for ${pointsToRedeem} points.`);
+
+            for (const userName in this.users) {
+                if (this.users.hasOwnProperty(userName)) {
+                    this.users[userName].totalScore = this.points;
+                    this.users[userName].redeemedProducts = this.users[userName].redeemedProducts || {};
+                    
+                    if (this.users[userName].redeemedProducts[selectedCardData.id]) {
+                        this.users[userName].redeemedProducts[selectedCardData.id]++;
+                    } else {
+                        this.users[userName].redeemedProducts[selectedCardData.id] = 1;
+                    }
+                }
+>>>>>>> parent of bc19a47 (Update UserPoints.js)
+            }
+            
             localStorage.setItem('users', JSON.stringify(this.users));
             this.displayPoints(); // Update the points display
         } else {
